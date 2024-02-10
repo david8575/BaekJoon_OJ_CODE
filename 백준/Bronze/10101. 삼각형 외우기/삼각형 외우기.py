dbb0b2acf7781ed5acfb7a01d92bbef84e532607
david8575 <103345490+david8575@users.noreply.github.com>
2024-02-11 00:29:31 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -0,0 +1,15 @@
+angles = []
+angles.append(int(input()))
+angles.append(int(input()))
+angles.append(int(input()))
+
+if angles[0] + angles[1] + angles[2] != 180:
+    print("Error")
+else:
+    if angles[0] == angles[1] and angles[1] == angles[2]:
+        print("Equilateral")
+    elif angles[0] != angles[1] and angles[1] != angles[2] and angles[0] != angles[2]:
+        print("Scalene")
+    else:
+        print("Isosceles")
+ 
